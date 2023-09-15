a51f31305348650e528a0dced41a118b0710a9e2
Darren Finch <finchdarren25@gmail.com>
2023-09-15 15:41:49 +0000

    Add .devcontainer.json file to prevent dubious ownership git warning and add bash script to set up environment

---
+++ setup-environment.sh
@@ -0,0 +1,19 @@
+# Install npm
+apt update -y
+apt upgrade -y
+apt install -y npm
+
+# Update npm
+apt-get install -y ca-certificates curl gnupg
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+apt-get update
+apt-get install nodejs -y
+
+# Install dependencies
+npm install
+
+# Run the app
+npm start
